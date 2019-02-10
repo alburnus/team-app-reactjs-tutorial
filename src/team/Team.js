691adfe99005478f92739c276279358134652a2c
Krzysztof <k.ukleja@gmail.com>
2019-02-10 22:23:45 +0100

    First fetch for Rest endpoint.

--- src/team/Team.js
+++ src/team/Team.js
@@ -2,11 +2,27 @@ import React from 'react';
 import TeamMessage from "./TeamMessage";
 
 export class Team extends React.Component {
+
+    constructor() {
+        super();
+        this.state = { teams: []}
+        this.fetchTeam();
+    }
+
+    fetchTeam() {
+        fetch('http://localhost:8080/api/team')
+            .then(result=>result.json())
+            .then(teams=>this.setState({teams}))
+
+    }
     render() {
         return (
             <div className="container">
                 <h1>Team!!</h1>
                 <TeamMessage message='Hi'/>
+                <ul>
+                    {this.state.teams.map(team=><li key={team.name}>{team.name}</li>)}
+                </ul>
             </div>
         )
     }
