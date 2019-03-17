b08eaed927bf0621186876218dae80c89ad33643
Krzysztof <k.ukleja@gmail.com>
2019-03-17 20:31:05 +0100

    Change from Class to Function, because I do not need state

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 
-import NewTeam from "./team/NewTeam";
+import {NewTeam} from "./team/NewTeam";
 import {Home} from "./Home";
 import {Menu} from "./menu/Menu";
 import Team from "./team/Team";
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <Menu/>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/team" component={Team}/>
-                    <Route exact path="/team/new" component={NewTeam}/>
-                </div>
-            </Router>
-        );
-    }
+export function App() {
+    return (
+        <Router>
+            <div>
+                <Menu/>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/team" component={Team}/>
+                <Route exact path="/team/new" component={NewTeam}/>
+            </div>
+        </Router>
+    );
 }
-
-export default App;
--- src/team/NewTeam.js
+++ src/team/NewTeam.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import NewTeamForm from './NewTeamForm';
 
-export class NewTeam extends React.Component {
-
-    render() {
-        return (
-            <div className="container">
-                <NewTeamForm/>
-            </div>
-        );
-    }
+export function NewTeam() {
+    return (
+        <div className="container">
+            <NewTeamForm/>
+        </div>
+    );
 }
 
-export default NewTeam;
